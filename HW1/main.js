2d20467f4b81bc7208f5399e15888fe68bc7989d
Vasya <vasya14stasiuk@gmail.com>
2020-02-10 04:15:05 +0200

    Rename rep.

--- HW1/main.js
+++ HW1/main.js
@@ -5,35 +5,36 @@ const milk = 90.2345;
 console.log("Maximum=", Math.max(15.678, 123.965, 90.2345));
 console.log("Minimum=", Math.min(bread, meat, milk));
 
-let allCost = bread + meat + milk;
+const allCost = bread + meat + milk;
 console.log("All products cost =", allCost);
 
 console.log("Integers cost =",Math.floor(bread) + Math.floor(meat) + Math.floor(milk));
 
-let sumRounded = (Math.ceil(bread / 100) * 100) + (Math.ceil(meat / 100) * 100) + (Math.ceil(milk / 100) * 100);
+const sumRounded = (Math.ceil(allCost / 100) * 100);
 console.log("Sum of rounded =", sumRounded);
 
-let floorSum = Math.floor(bread) + Math.floor(meat) + Math.floor(milk);
+const floorSum = Math.floor(bread) + Math.floor(meat) + Math.floor(milk);
 if(floorSum % 2 === 0){
     console.log(true);
 } else {
 console.log(false);
 }
 
-let rest = 500- (bread + meat + milk);
-console.log (rest);
+const rest = 500- (bread + meat + milk);
+console.log ("Rest =", rest);
 
-let middleValue = allCost / 3;
-console.log(middleValue.toFixed(2));
+const middleValue = (allCost / 3).toFixed(2);
+console.log("Middle value =", middleValue);
 
-let discount = Math.random() * 10;
-console.log("Discount is", discount, "%");
-let lastPrice = 100 - discount;
-let needPay = (allCost/100) * lastPrice;
+const discount = Math.random() * 10;
+console.log("Discount is", discount.toFixed(2), "%");
+const lastPrice = 100 - discount;
+const needPay = (allCost/100) * lastPrice;
 console.log("Price with discount =",needPay.toFixed(2));
-let clearSum = allCost / 2;
-let discountNumber = (allCost/100) * discount;
-console.log("Clear money =", clearSum - discountNumber);
+const clearSum = allCost / 2;
+const discountNumber = (allCost/100) * discount;
+const finishSum = (clearSum - discountNumber).toFixed(2);
+console.log("Clear money =", finishSum);
 
 const textTemplate = `Максимальне число: ${Math.max(15.678, 123.965, 90.2345)},
  мінімальне число: ${Math.min(bread, meat, milk)},
@@ -41,6 +42,7 @@ const textTemplate = `Максимальне число: ${Math.max(15.678, 123.
    вартість цілих частин: ${Math.floor(bread) + Math.floor(meat) + Math.floor(milk)},
     заокруглені до сотень: ${sumRounded},
     сума решти: ${rest},
-    середнє: ${middleValue.toFixed(2)}
+    середнє: ${middleValue}
     сума до оплати зі знижкою: ${needPay.toFixed(2)}
-    чистий прибуток: ${clearSum - discountNumber}`;
+    чистий прибуток: ${finishSum}`;
+console.log(textTemplate)
